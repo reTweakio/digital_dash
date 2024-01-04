5b504dec439d88b775bd94d756b83fad666ee569
Alec Broadhurst <alec@Alecs-MacBook-Air.local>
2024-01-04 14:01:37 -0600

    removed tokio and using std instead, all telemetry data is working properly

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
-use tokio::net::UdpSocket;
+use std::net::UdpSocket;
 
-#[tokio::main]
-async fn main() {
-    let local_addr = "10.0.0.58:8080";
-    let socket = UdpSocket::bind(&local_addr).await.expect("Failed to bind to address");
+struct PacketInfo {
+    current_rpm: f32,
+    speed: f32,
+    best_lap: f32,
+    current_lap: f32,
+    current_race_time: f32,
+    gear: u8
+}
+
+
+fn main() {
+    let local_addr: &str = "10.0.0.58:8080";
+    let socket: UdpSocket = UdpSocket::bind(&local_addr).expect("Failed to bind to address");
 
     println!("Listening on: {}", local_addr);
 
     loop {
         let mut buf: Vec<u8> = vec![0; 1024];
-        let (len, src) = socket.recv_from(&mut buf).await.expect("Failed to receive data");
+        socket.recv_from(&mut buf).expect("Failed to receive data");
 
-        println!("Received {} bytes from {}: {:?}", len, src, &buf[..len]);
+        let packet_info = PacketInfo {
+            current_rpm: f32::from_le_bytes(buf[16..20].try_into().unwrap()),
+            speed: f32::from_le_bytes(buf[244..248].try_into().unwrap()),
+            best_lap: f32::from_le_bytes(buf[284..288].try_into().unwrap()),
+            current_lap: f32::from_le_bytes(buf[292..296].try_into().unwrap()),
+            current_race_time: f32::from_le_bytes(buf[296..300].try_into().unwrap()),
+            gear: buf[307] as u8
+        };
+        
     }
 }
