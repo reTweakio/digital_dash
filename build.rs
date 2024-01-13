79a4ed2ca6eff903b546a5e80614f7937fb77722
retweakio <retweakio@anonymous.>
2024-01-12 21:31:24 -0600

    started working on adding ui with slint

---
+++ build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    slint_build::compile("/home/retweakio/__rust_code__/forza_telemetry/ui/dashboard.slint").unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::UdpSocket;
+slint::include_modules!();
 
 struct PacketInfo {
     current_rpm: f32,
@@ -9,25 +10,28 @@ struct PacketInfo {
     gear: u8
 }
 
+fn main() -> Result<(), slint::PlatformError> {
+    let ui: Dashboard = Dashboard::new()?;
 
-fn main() {
     let local_addr: &str = "10.0.0.58:8080";
     let socket: UdpSocket = UdpSocket::bind(&local_addr).expect("Failed to bind to address");
 
     println!("Listening on: {}", local_addr);
 
-    loop {
-        let mut buf: Vec<u8> = vec![0; 1024];
-        socket.recv_from(&mut buf).expect("Failed to receive data");
+    // loop {
+    //     let mut buf: Vec<u8> = vec![0; 1024];
+    //     socket.recv_from(&mut buf).expect("Failed to receive data");
 
-        let packet_info = PacketInfo {
-            current_rpm: f32::from_le_bytes(buf[16..20].try_into().unwrap()),
-            speed: f32::from_le_bytes(buf[244..248].try_into().unwrap()),
-            best_lap: f32::from_le_bytes(buf[284..288].try_into().unwrap()),
-            current_lap: f32::from_le_bytes(buf[292..296].try_into().unwrap()),
-            current_race_time: f32::from_le_bytes(buf[296..300].try_into().unwrap()),
-            gear: buf[307] as u8
-        };
+    //     let packet_info = PacketInfo {
+    //         current_rpm: f32::from_le_bytes(buf[16..20].try_into().unwrap()),
+    //         speed: f32::from_le_bytes(buf[244..248].try_into().unwrap()),
+    //         best_lap: f32::from_le_bytes(buf[284..288].try_into().unwrap()),
+    //         current_lap: f32::from_le_bytes(buf[292..296].try_into().unwrap()),
+    //         current_race_time: f32::from_le_bytes(buf[296..300].try_into().unwrap()),
+    //         gear: buf[307] as u8
+    //     };
         
-    }
+    // }
+
+    ui.run()
 }
