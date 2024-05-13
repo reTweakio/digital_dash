d256d139f33e66d4f5765dfba32659d9e78a3ec3
Alec Broadhurst <alec@Alecs-MacBook-Air.local>
2024-05-12 19:06:21 -0500

    running networking function on seperate thread, created a channel for threads to share data

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,15 @@
-use std::net::UdpSocket;
-slint::include_modules!();
+use std::thread;
+use std::sync::mpsc;
 
-struct PacketInfo {
-    current_rpm: f32,
-    speed: f32,
-    best_lap: f32,
-    current_lap: f32,
-    current_race_time: f32,
-    gear: u8
-}
+mod networking;
+mod ui;
 
-fn main() -> Result<(), slint::PlatformError> {
-    let ui: Dashboard = Dashboard::new()?;
+fn main() {
+    let (sender, receiver) = mpsc::channel();
 
-    let local_addr: &str = "10.0.0.58:8080";
-    //let socket: UdpSocket = UdpSocket::bind(&local_addr).expect("Failed to bind to address");
+    thread::spawn(move || {
+        networking::parse_packets(sender);
+    });
 
-    println!("stening on: {}", local_addr);
-
-    // loop {
-    //     let mut buf: Vec<u8> = vec![0; 1024];
-    //     socket.recv_from(&mut buf).expect("Failed to receive data");
-
-    //     let packet_info = PacketInfo {
-    //         current_rpm: f32::from_le_bytes(buf[16..20].try_into().unwrap()),
-    //         speed: f32::from_le_bytes(buf[244..248].try_into().unwrap()),
-    //         best_lap: f32::from_le_bytes(buf[284..288].try_into().unwrap()),
-    //         current_lap: f32::from_le_bytes(buf[292..296].try_into().unwrap()),
-    //         current_race_time: f32::from_le_bytes(buf[296..300].try_into().unwrap()),
-    //         gear: buf[307] as u8
-    //     };
-        
-    // }
-
-    ui.run()
+    ui::run_ui(receiver);
 }
--- src/networking.rs
+++ src/networking.rs
@@ -1,8 +1,9 @@
 use std::net::UdpSocket;
+use std::sync::mpsc::Sender;
 use local_ip_address::local_ip;
 
 
-struct PacketInfo {
+pub struct PacketInfo {
     current_rpm: f32,
     speed: f32,
     best_lap: f32,
@@ -16,10 +17,11 @@ fn setup_udp_socket() -> UdpSocket {
     let port: &str = "8080";
     let binding_addr: String = format!("{}:{}", ip_addr, port);
     let socket: UdpSocket = UdpSocket::bind(binding_addr).expect("Failed to bind to address");
+    println!("Successfully binded to udp socket");
     return socket
 }
 
-pub fn parse_packets() {
+pub fn parse_packets(sender: Sender<PacketInfo>) -> PacketInfo {
     let socket: UdpSocket = setup_udp_socket();
 
     loop {
@@ -34,5 +36,7 @@ pub fn parse_packets() {
             current_race_time: f32::from_le_bytes(buf[296..300].try_into().unwrap()),
             gear: buf[307] as u8
         };
+
+        sender.send(packet_info).expect("Error sending packet data to thread");
     }
 }
