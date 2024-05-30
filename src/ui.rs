3c9b5c5b69225750d572ba3f603d364815d3fa4e
Alec Broadhurst <alec@Alecs-MacBook-Air.local>
2024-05-30 13:46:31 -0500

    updated led lights to be a little bit more realistic

--- src/ui.rs
+++ src/ui.rs
@@ -6,14 +6,13 @@ use crate::networking::PacketInfo;
 slint::include_modules!();
 
 fn update_rpm_lights(rpm: f32, max_rpm: f32, rpm_lights: &mut Vec<bool>) {
-    let step_size: f32 = max_rpm / rpm_lights.len() as f32;
-    let lights_on = (rpm / step_size).ceil() as usize;
-
+    let starting_rpm: f32 = max_rpm * 0.5;
+    let step_size: f32 = ((max_rpm - 1000.0) - starting_rpm) / (rpm_lights.len() as f32);
+    
     for (i, on_status) in rpm_lights.iter_mut().enumerate() {
-        *on_status = i < lights_on;
+        *on_status = rpm >= starting_rpm + (step_size * i as f32);
     }
 }
-
 
 pub fn run_ui(receiver: Receiver<PacketInfo>) {
     let dashboard: Dashboard = Dashboard::new().unwrap();
