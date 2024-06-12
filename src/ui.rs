3b1051d77da3a010adfb4fad767ad99cecc6a224
Alec Broadhurst <alec@Alecs-MacBook-Air.local>
2024-06-12 14:54:18 -0500

    added some error handling to ui.rs

--- src/ui.rs
+++ src/ui.rs
@@ -15,59 +15,81 @@ fn update_rpm_lights(rpm: f32, max_rpm: f32, rpm_lights: &mut Vec<bool>) {
 }
 
 pub fn run_ui(receiver: Receiver<PacketInfo>) {
-    let dashboard: Dashboard = Dashboard::new().unwrap();
+    let dashboard: Dashboard = match Dashboard::new() {
+        Ok(dash) => dash,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            std::process::exit(1);
+        }
+    };
     let weak_dashboard: slint::Weak<Dashboard> = dashboard.as_weak();
 
-    thread::spawn(move || {
-        loop {
-            let packet_info: PacketInfo = receiver.recv().unwrap();
-
-            let current_rpm: f32 = packet_info.get_current_rpm();
-            let max_rpm: f32 = packet_info.get_max_rpm();
-            let speed: f32 = packet_info.get_speed();
-            let best_lap: String = packet_info.get_best_lap();
-            let current_lap: String = packet_info.get_current_lap();
-            let gear: i32 = packet_info.get_gear();
-            let accel: f32 = packet_info.get_accel();
-            let brake: f32 = packet_info.get_brake();
-            let position: i32 = packet_info.get_position();
-            let temp_left_f: f32 = packet_info.get_temp_left_f();
-            let temp_right_f: f32 = packet_info.get_temp_right_f();
-            let temp_left_r: f32 = packet_info.get_temp_left_r();
-            let temp_right_r: f32 = packet_info.get_temp_right_r();
-            let lap_number: i32 = packet_info.get_lap_number();
-
-            let mut delta: String = String::from("");
-            let mut new_best: bool = true;
-
-            if packet_info.get_last_lap().is_some() {
-                delta = packet_info.get_delta();
-                new_best = delta.starts_with('-');
+    thread::spawn(move || loop {
+        let packet_info: PacketInfo = match receiver.recv() {
+            Ok(recved_info) => recved_info,
+            Err(err) => {
+                eprintln!("Error: {}", err);
+                std::process::exit(1);
             }
+        };
 
-            let mut rpm_lights: Vec<bool> = vec![false; 15];
-            update_rpm_lights(current_rpm, max_rpm, &mut rpm_lights);
+        let current_rpm: f32 = packet_info.get_current_rpm();
+        let max_rpm: f32 = packet_info.get_max_rpm();
+        let speed: f32 = packet_info.get_speed();
+        let best_lap: String = packet_info.get_best_lap();
+        let current_lap: String = packet_info.get_current_lap();
+        let gear: i32 = packet_info.get_gear();
+        let accel: f32 = packet_info.get_accel();
+        let brake: f32 = packet_info.get_brake();
+        let position: i32 = packet_info.get_position();
+        let temp_left_f: f32 = packet_info.get_temp_left_f();
+        let temp_right_f: f32 = packet_info.get_temp_right_f();
+        let temp_left_r: f32 = packet_info.get_temp_left_r();
+        let temp_right_r: f32 = packet_info.get_temp_right_r();
+        let lap_number: i32 = packet_info.get_lap_number();
 
-            weak_dashboard.upgrade_in_event_loop(move |dash: Dashboard| {
-                dash.set_rpm(current_rpm);
-                dash.set_speed(speed);
-                dash.set_best_lap(SharedString::from(best_lap));
-                dash.set_current_lap(SharedString::from(current_lap));
-                dash.set_gear(gear);
-                dash.set_accel(accel);
-                dash.set_brake(brake);
-                dash.set_position(position);
-                dash.set_temp_left_f(temp_left_f);
-                dash.set_temp_right_f(temp_right_f);
-                dash.set_temp_left_r(temp_left_r);
-                dash.set_temp_right_r(temp_right_r);
-                dash.set_lap_number(lap_number);
-                dash.set_rpm_lights(ModelRc::new(VecModel::from(rpm_lights)));
-                dash.set_delta(SharedString::from(delta));
-                dash.set_new_best(new_best);
-                }).unwrap();
+        let mut delta: String = String::from("");
+        let mut new_best: bool = true;
+
+        if packet_info.get_last_lap().is_some() {
+            delta = packet_info.get_delta();
+            new_best = delta.starts_with('-');
+        }
+
+        let mut rpm_lights: Vec<bool> = vec![false; 15];
+        update_rpm_lights(current_rpm, max_rpm, &mut rpm_lights);
+
+        match weak_dashboard.upgrade_in_event_loop(move |dash: Dashboard| {
+            dash.set_rpm(current_rpm);
+            dash.set_speed(speed);
+            dash.set_best_lap(SharedString::from(best_lap));
+            dash.set_current_lap(SharedString::from(current_lap));
+            dash.set_gear(gear);
+            dash.set_accel(accel);
+            dash.set_brake(brake);
+            dash.set_position(position);
+            dash.set_temp_left_f(temp_left_f);
+            dash.set_temp_right_f(temp_right_f);
+            dash.set_temp_left_r(temp_left_r);
+            dash.set_temp_right_r(temp_right_r);
+            dash.set_lap_number(lap_number);
+            dash.set_rpm_lights(ModelRc::new(VecModel::from(rpm_lights)));
+            dash.set_delta(SharedString::from(delta));
+            dash.set_new_best(new_best);
+        }) {
+            Ok(_) => (),
+            Err(err) => {
+                eprintln!("Error: {}", err);
+                std::process::exit(1);
+            }
         }
     });
 
-    dashboard.run().unwrap();
+    match dashboard.run() {
+        Ok(_) => (),
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            std::process::exit(1);
+        }
+    }
 }
