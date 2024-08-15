9a43c383f51817aa52953eb7cbea3494c73dc94f
reTweakio <alecrbroad@gmail.com>
2024-08-15 13:27:30 -0500

    moved loop back into parse_packets function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::sync::mpsc;
 use std::thread;
 
+use telemetry::Telemetry;
+
 mod telemetry;
 mod ui;
 
 fn main() {
     let (sender, receiver) = mpsc::channel();
 
-    thread::spawn(move || loop {
-        telemetry::Telemetry::parse_packets(&sender);
+    thread::spawn(move || {
+        Telemetry::parse_packets(&sender);
     });
 
     ui::run_ui(receiver);
--- src/telemetry.rs
+++ src/telemetry.rs
@@ -2,6 +2,7 @@ use local_ip_address::local_ip;
 use std::net::UdpSocket;
 use std::sync::mpsc::Sender;
 
+#[derive(Clone)]
 pub struct Telemetry {
     current_rpm: f32,
     max_rpm: f32,
@@ -118,7 +119,7 @@ impl Telemetry {
 
     pub fn get_delta(&self) -> String {
         let delta = if self.last_lap == self.best_lap {
-            self.last_lap - self.prev_best
+            self.prev_best - self.best_lap
         } else {
             self.last_lap - self.best_lap
         };
@@ -180,66 +181,48 @@ impl Telemetry {
 
     pub fn parse_packets(sender: &Sender<Telemetry>) {
         let socket: UdpSocket = Self::setup_udp_socket();
-        let mut buf: Vec<u8> = vec![0; 500];
 
-        match socket.recv_from(&mut buf) {
-            Ok(_) => (),
-            Err(err) => {
-                eprintln!("Error: {}", err);
-                std::process::exit(1);
-            }
-        }
-
-        let mut prev_best: f32 = 0.0;
-
-        let current_rpm: f32 = Self::parse_f32_from_bytes(&buf[16..20]).round();
-        let max_rpm: f32 = Self::parse_f32_from_bytes(&buf[8..12]);
-        let speed: f32 = (Self::parse_f32_from_bytes(&buf[244..248]) * 2.237).round();
-        let best_lap: f32 = Self::parse_f32_from_bytes(&buf[284..288]);
-        let current_lap: f32 = Self::parse_f32_from_bytes(&buf[292..296]);
-        let last_lap: f32 = Self::parse_f32_from_bytes(&buf[288..292]);
-        let lap_number: i32 = Self::parse_i16_from_bytes(&buf[300..302]) as i32;
-        let position: i32 = buf[302] as i32;
-        let gear: i32 = buf[307] as i32;
-        let accel: f32 = buf[303] as f32;
-        let brake: f32 = buf[304] as f32;
-        let temp_left_f: f32 = Self::parse_f32_from_bytes(&buf[256..260]).round();
-        let temp_right_f: f32 = Self::parse_f32_from_bytes(&buf[260..264]).round();
-        let temp_left_r: f32 = Self::parse_f32_from_bytes(&buf[264..268]).round();
-        let temp_right_r: f32 = Self::parse_f32_from_bytes(&buf[268..272]).round();
-
-        if lap_number == 2 {
-            prev_best = best_lap;
-        }
-
-        if last_lap != best_lap {
-            prev_best = best_lap;
-        }
-
-        let telem: Telemetry = Telemetry::new(
-            current_rpm,
-            max_rpm,
-            speed,
-            best_lap,
-            prev_best,
-            current_lap,
-            last_lap,
-            gear,
-            accel,
-            brake,
-            position,
-            temp_left_f,
-            temp_right_f,
-            temp_left_r,
-            temp_right_r,
-            lap_number,
+        let mut telem = Telemetry::new(
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 0, 0.0, 0.0, 0.0, 0.0, 0,
         );
 
-        match sender.send(telem) {
-            Ok(()) => (),
-            Err(err) => {
-                eprintln!("Error: {}", err);
-                std::process::exit(1);
+        loop {
+            let mut buf: Vec<u8> = vec![0; 500];
+
+            match socket.recv_from(&mut buf) {
+                Ok(_) => (),
+                Err(err) => {
+                    eprintln!("Error: {}", err);
+                    std::process::exit(1);
+                }
+            }
+
+            telem.current_rpm = Self::parse_f32_from_bytes(&buf[16..20]).round();
+            telem.max_rpm = Self::parse_f32_from_bytes(&buf[8..12]);
+            telem.speed = (Self::parse_f32_from_bytes(&buf[244..248]) * 2.237).round();
+            telem.best_lap = Self::parse_f32_from_bytes(&buf[284..288]);
+            telem.current_lap = Self::parse_f32_from_bytes(&buf[292..296]);
+            telem.last_lap = Self::parse_f32_from_bytes(&buf[288..292]);
+            telem.lap_number = Self::parse_i16_from_bytes(&buf[300..302]) as i32;
+            telem.position = buf[302] as i32;
+            telem.gear = buf[307] as i32;
+            telem.accel = buf[303] as f32;
+            telem.brake = buf[304] as f32;
+            telem.temp_left_f = Self::parse_f32_from_bytes(&buf[256..260]).round();
+            telem.temp_right_f = Self::parse_f32_from_bytes(&buf[260..264]).round();
+            telem.temp_left_r = Self::parse_f32_from_bytes(&buf[264..268]).round();
+            telem.temp_right_r = Self::parse_f32_from_bytes(&buf[268..272]).round();
+
+            if telem.last_lap != telem.best_lap {
+                telem.prev_best = telem.best_lap;
+            }
+
+            match sender.send(telem.clone()) {
+                Ok(()) => (),
+                Err(err) => {
+                    eprintln!("Error: {}", err);
+                    std::process::exit(1);
+                }
             }
         }
     }
