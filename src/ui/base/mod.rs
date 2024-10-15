86c9ecc7b695ee2d03ca7c13874d992f1e2717be
reTweakio <alecrbroad@gmail.com>
2024-10-15 09:03:02 -0500

    renamed and removed some files in ui directory

--- src/ui/base/mod.rs
+++
@@ -1,8 +0,0 @@
-fn update_rpm_lights(rpm: f32, max_rpm: f32, rpm_lights: &mut Vec<bool>) {
-    let starting_rpm: f32 = max_rpm * 0.5;
-    let step_size: f32 = ((max_rpm - 1000.0) - starting_rpm) / (rpm_lights.len() as f32);
-
-    for (i, on_status) in rpm_lights.iter_mut().enumerate() {
-        *on_status = rpm >= starting_rpm + (step_size * i as f32);
-    }
-}
--- src/ui/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod base;
-pub mod forza_ui;
